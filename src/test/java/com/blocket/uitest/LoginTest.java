168e785d771eefaad3c0aab7aa1af60edba1b4e3
Kavsjö <victor.kavsjo@sogeti.com>
2019-10-29 13:16:27 +0100

    Data providers

--- src/test/java/com/blocket/uitest/LoginTest.java
+++ src/test/java/com/blocket/uitest/LoginTest.java
@@ -1,42 +1,51 @@
 package com.blocket.uitest;
 
+import java.util.Map;
+
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import com.blocket.base.BaseTest;
+import com.blocket.base.DataProviders;
 import com.blocket.pages.LoginPage;
 import com.blocket.pages.StartPage;
 
 
 public class LoginTest extends BaseTest {
-	@Test
-	public void validLoginTest(){
+	@Test(dataProvider = "csvReader", dataProviderClass = DataProviders.class)
+	public void validLoginTest(Map<String, String> testData) {
+		
+		String username = testData.get("username");
+		String password = testData.get("password");
+		
 		StartPage startPage = new StartPage(driver, log);
 		startPage.openPageUrl();
 		
 		LoginPage loginPage = startPage.pressLoginLink();
 		
-		StartPage loggedIn = loginPage.login("landa@hotmail.com", "landa1234");
+		StartPage loggedIn = loginPage.login(username, password);
 		
 		Assert.assertTrue(loggedIn.isLogOutButtonVisibile());
 		
 		}	
 	 
-	@Test
-	public void invalidLoginTest() {
+	@Test(dataProvider = "csvReader", dataProviderClass = DataProviders.class)
+	public void invalidLoginTest(Map<String, String> testData) {
+		
+		String invalidUsername = testData.get("username");
+		String invalidPassword = testData.get("password");
+		String expectedMessage = testData.get("message");
+		
 		StartPage startPage = new StartPage(driver, log);
 		startPage.openPageUrl();
 		
 		LoginPage loginPage = startPage.pressLoginLink();
-		loginPage.invalidLogIn("invalid@hotmail.com", "BadPassword");
+		loginPage.invalidLogIn(invalidUsername, invalidPassword);
 		
 		loginPage.waitForErrorMessage();
 		String message = loginPage.getLoginErrorMessage();
 		
-		String expectedMessage = "Login was unsuccessful. Please correct the errors and try again.";
-		
 		Assert.assertTrue(message.contains(expectedMessage), "Message was wrong");
-	
 	}
 	
 
